596532dc8bd28f795c0fdd80f6155b8ba84d2ffa
Ian Kaliszewski <ikalisz@yahoo.com>
2019-04-30 09:37:13 -0600

    finished app-2

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
+//center
 class App extends Component {
   constructor() {
     super()
--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state ={
+      arrItems : ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese']
+    }
+  }
+  
   render() {
+    const arrItemsDisplay = this.state.arrItems.map((food, i) => {
+      return <li key={i}>{food}</li>
+    })
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <ul>
+          {arrItemsDisplay}
+        </ul>
       </div>
     );
   }
